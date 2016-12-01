f4b2c907e95b05b181dc8c01e5653c112bfd3c6d
kao <kalle.ott@cap3.de>
2016-12-01 01:14:22 +0100

    added test example with mocking

--- src/__tests__/components/MarkdownText.test.tsx
+++ src/__tests__/components/MarkdownText.test.tsx
@@ -3,6 +3,7 @@ import renderer from 'react-test-renderer'
 import { MarkdownText } from 'components/MarkdownText'
 
 describe('MarkdownText shall render text', () => {
+
     const textValue = '# headline\ntext'
 
     const component = renderer.create(
---
+++ src/__tests__/components/TextArea.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import renderer from 'react-test-renderer'
+import { TextArea } from 'components/TextArea'
+
+jest.mock('react-toolbox/lib/input/Input', () => { return { inputFactory: () => 'Input' } })
+
+describe('TextArea shall render text', () => {
+
+    const textValue = '# headline\ntext'
+    const onChange = jest.fn()
+    const component = renderer.create(<TextArea value={textValue} onChange={onChange} />)
+    let tree = component.toJSON()
+
+    test('Snapshot test', () => {
+
+        expect(tree).toMatchSnapshot()
+    })
+
+    test('props value should be textValue', () => {
+
+        expect(component.getInstance().props.value).toBe(textValue)
+    })
+
+    test('props value should be in component state', () => {
+        const textArea = component.getInstance() as any as TextArea
+
+        expect(textArea.state.value).toEqual(textValue)
+    })
+
+    test('state should update on props update', () => {
+        const textArea = component.getInstance() as any as TextArea
+        textArea.componentWillReceiveProps({ value: 'next text', onChange: onChange })
+
+        expect(textArea.state.value).toEqual('next text')
+    })
+})
