d421d0d386b8368d903c3a865b9ebb202f76ea5d
Kalle Ott <kalle.ott@cap3.de>
2017-04-21 11:05:52 +0200

    more copy paste fixes

--- hotReloadingServer.js
+++ hotReloadingServer.js
@@ -1,15 +1,17 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+const app = new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
     historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || "localhost", (err, result) => {
     if (err) {
         return console.log(err);
     }
-
-    console.log('Listening at http://localhost:3000/');
+    const addr = app.address();
+    console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
--- server.js
+++ server.js
@@ -8,7 +8,10 @@ app.get('/', function (req, res) {
     res.sendFile(path.resolve('./index.html'));
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", () => {
-    var addr = server.address();
+app.listen(process.env.PORT || 3000, process.env.IP || "localhost", (err, result) => {
+    if (err) {
+        return console.log(err);
+    }
+    const addr = app.address();
     console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
